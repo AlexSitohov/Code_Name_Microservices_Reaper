78618ffead94de5c24ca03c184bb247cd511f0c7
AlexSitohov <lilinfinitemob@gmail.com>
2023-06-15 18:27:24 +0300

    [ADD] pydantic schemas for users

---
+++ schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from uuid import UUID
+
+
+class UserSchema(BaseModel):
+    id: UUID
+    username: str
+    first_name: str
+    last_name: str
+    password: str
+    email: str
+    phone: str
+    city: str
+
+    class Config:
+        orm_mode = True
