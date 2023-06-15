2f8669e42427e685845a485a222ed586ee272a28
AlexSitohov <lilinfinitemob@gmail.com>
2023-06-15 18:02:52 +0300

    [ADD] setup database config

---
+++ database_config.py
@@ -0,0 +1,22 @@
+from typing import Generator
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://user:password@localhost/db"
+
+# create async engine for interaction with database
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+
+# create session for the interaction with database
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, future=True)
+
+
+async def get_db() -> Generator:
+    """Dependency for getting async session"""
+    try:
+        session: AsyncSession = SessionLocal()
+        yield session
+    finally:
+        await session.close()
