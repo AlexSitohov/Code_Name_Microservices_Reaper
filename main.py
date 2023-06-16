57704ad800c3b295087b5069e6a25022f6c1343c
AlexSitohov <lilinfinitemob@gmail.com>
2023-06-16 14:36:20 +0300

    [ADD] create user functional

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database_config import get_db
 import models
+from hash import hash_password
+from schemas import UserSchema, UserCreateSchema
 
 app = FastAPI()
 
@@ -22,8 +24,18 @@ async def ping():
     return {"message": "Hello World"}
 
 
-@app.get("/users", status_code=status.HTTP_201_CREATED)
+@app.get("/users", status_code=status.HTTP_201_CREATED, response_model=list[UserSchema])
 async def get_users_list(session: AsyncSession = Depends(get_db)):
     users_list = await session.execute(
         select(models.User))
     return users_list.scalars().all()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED)
+async def create_user(user_data: UserCreateSchema, session: AsyncSession = Depends(get_db)):
+    user_data.password = await hash_password(user_data.password)
+    new_user = models.User(**user_data.dict())
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+    return new_user
--- schemas.py
+++ schemas.py
@@ -14,3 +14,16 @@ class UserSchema(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class UserCreateSchema(BaseModel):
+    username: str
+    first_name: str
+    last_name: str
+    password: str
+    email: str
+    phone: str
+    city: str
+
+    class Config:
+        orm_mode = True
