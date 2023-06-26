5fcd2a24a302225556c2894a75da038c13e92938
AlexSitohov <lilinfinitemob@gmail.com>
2023-06-26 16:09:28 +0300

    [ADD] New column email_confirmed_date_time in users table

--- users_service/models.py
+++ users_service/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, String, TIMESTAMP, text
+from sqlalchemy import Column, String, TIMESTAMP, text, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 import uuid as _uuid
+from datetime import datetime
 
 from sqlalchemy.orm import declarative_base
 
@@ -14,8 +15,9 @@ class User(Base):
     username = Column(String(50), unique=True, nullable=False)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
-    password = Column(String)
+    password = Column(String, nullable=False)
     email = Column(String(30), unique=True)
     phone = Column(String, unique=True)
-    city = Column(String(30))
+    city = Column(String(30), nullable=True)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    email_confirmed_date_time = Column(DateTime, nullable=True)
