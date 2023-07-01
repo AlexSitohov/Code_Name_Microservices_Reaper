e7b9678c243d6d0af7bf9a21247127cdb7dc4302
AlexSitohov <lilinfinitemob@gmail.com>
2023-07-01 12:16:16 +0300

    [ADD] api gateway redirect from / to /docs

--- api_gateway/main.py
+++ api_gateway/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 from routers import users, auth
-
 
 app = FastAPI(title='api_gateway')
 
@@ -12,6 +12,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
+
+@app.get("/")
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
 
 app.include_router(users.router)
 app.include_router(auth.router)
